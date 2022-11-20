b597e76ec9cc995969665b12dce5803f7b13049a
Dilber KILIÇ <48033000+dilberkilic@users.noreply.github.com>
2022-11-20 18:31:21 +0300

    Update app.js
    orders route eklendi.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
-const productsRouter= require('./api/routers/products');
+const productsRoute= require('./api/routers/products');
+const ordersRoute=require('./api/routers/orders');
 
 /*app.use((req,res,next)=>{
     res.status(200).json({
@@ -8,6 +9,7 @@ const productsRouter= require('./api/routers/products');
     });
 });*/
 
-app.use('/products',productsRouter);
+app.use('/products',productsRoute);
+app.use('/orders',ordersRoute);
 
 module.exports = app;
